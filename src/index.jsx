1aaace53e852a8f6d6cd280fed08fef05756f6bb
Durant Ratcliff <durantr1234@gmail.com>
2025-07-29 19:58:48 -0400

    Resolve merge conflicts

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './styles.css'; // Import your global styles
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root') // Matches the <div id="root"></div> in index.html
+);
